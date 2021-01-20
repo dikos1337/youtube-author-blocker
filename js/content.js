6a16bca8dced266d9d50234b8083431d1fc1e067
dikos1337 <59239573+dikos1337@users.noreply.github.com>
2021-01-20 15:43:16 +0300

    code style formatting

--- js/content.js
+++ js/content.js
@@ -1,51 +1,51 @@
 function hideAuthors(blockedAuthors) {
-    // List of blocks with video
-    $("ytd-video-renderer").each(
-        function () {
-            let currentBlock = $(this);
+  // List of blocks with video
+  $("ytd-video-renderer").each(function () {
+    let currentBlock = $(this);
 
-            // List of links to video authors
-            let authors = currentBlock.find("a[class='yt-simple-endpoint style-scope yt-formatted-string']")
+    // List of links to video authors
+    let authors = currentBlock.find(
+      "a[class='yt-simple-endpoint style-scope yt-formatted-string']"
+    );
 
-            // Removing an author from the list of blocks with a video if his nickname is in the banned list
-            authors.each(function () {
-                if (blockedAuthors.includes($(this).text())) {
-                    currentBlock.detach();
-                    // console.log($(this).text() + " is blocked")
-                }
-            })
-        }
-    )
+    // Removing an author from the list of blocks with a video if his nickname is in the banned list
+    authors.each(function () {
+      if (blockedAuthors.includes($(this).text())) {
+        currentBlock.detach();
+        // console.log($(this).text() + " is blocked")
+      }
+    });
+  });
 }
 
 function getDataAndHideAuthors() {
-    chrome.storage.sync.get('blockedAuthors', function (data) {
-        const blockedAuthors = data["blockedAuthors"] || [];
-        hideAuthors(blockedAuthors)
-    });
+  chrome.storage.sync.get("blockedAuthors", function (data) {
+    const blockedAuthors = data["blockedAuthors"] || [];
+    hideAuthors(blockedAuthors);
+  });
 }
 
 function waitForEl(selector, callback, maxtries = false, interval = 100) {
-    const poller = setInterval(() => {
-        const el = jQuery(selector)
-        const retry = maxtries === false || maxtries-- > 0
-        if (retry && el.length < 1) return // will try again
-        clearInterval(poller)
-        callback(el || null)
-    }, interval)
+  const poller = setInterval(() => {
+    const el = jQuery(selector);
+    const retry = maxtries === false || maxtries-- > 0;
+    if (retry && el.length < 1) return; // will try again
+    clearInterval(poller);
+    callback(el || null);
+  }, interval);
 }
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    if (request.message.changeInfo.status === 'complete') {
-        if (window.location.href.includes("/feed/trending")) {
-            let pollerCounter = 0
-            let eventPoller = setInterval(function () {
-                waitForEl("ytd-video-renderer", getDataAndHideAuthors);
-                pollerCounter++
-                if (pollerCounter >= 10) {
-                    clearInterval(eventPoller)
-                }
-            }, 100)
+  if (request.message.changeInfo.status === "complete") {
+    if (window.location.href.includes("/feed/trending")) {
+      let pollerCounter = 0;
+      let eventPoller = setInterval(function () {
+        waitForEl("ytd-video-renderer", getDataAndHideAuthors);
+        pollerCounter++;
+        if (pollerCounter >= 10) {
+          clearInterval(eventPoller);
         }
+      }, 100);
     }
-})
+  }
+});
--- js/popup.js
+++ js/popup.js
@@ -1,47 +1,54 @@
 const App = {
-    data() {
-        return {
-            inputUsername: "",
-            localUserList: []
-        }
-    },
-    methods: {
-        deleteUserFromChromeStorage(username) {
-            chrome.storage.sync.get("blockedAuthors", data => {
-                let tmpUserList = data["blockedAuthors"] || [];
-                tmpUserList = tmpUserList.filter(item => item != username);
-                chrome.storage.sync.set({ ["blockedAuthors"]: tmpUserList }, function () { });
-                this.updateLocalUserList();
-            });
-        },
-        addUserToChromeStorage() {
-            if (this.inputUsername.trim()) {  // checking that the input is not empty
-                chrome.storage.sync.get("blockedAuthors", data => {
-                    const tmpUserList = data["blockedAuthors"] || [];
-                    tmpUserList.push(this.inputUsername);
-                    chrome.storage.sync.set({ ["blockedAuthors"]: tmpUserList }, function () { });
-                    this.inputUsername = "";  // Clear input
-                    this.updateLocalUserList();
-                });
-            }
-        },
-        updateLocalUserList() {
-            chrome.storage.sync.get("blockedAuthors", data => {
-                const tmpUserList = data["blockedAuthors"] || [];
-                this.localUserList = tmpUserList;
-            })
-        }
-    },
-    watch: {
-        inputUsername(value) {
-            if (value.length >= 30) {
-                this.inputUsername = this.inputUsername.slice(0, 30); // max length is 30
-            }
-        }
-    },
-    mounted: function () {
+  data() {
+    return {
+      inputUsername: "",
+      localUserList: [],
+    };
+  },
+  methods: {
+    deleteUserFromChromeStorage(username) {
+      chrome.storage.sync.get("blockedAuthors", (data) => {
+        let tmpUserList = data["blockedAuthors"] || [];
+        tmpUserList = tmpUserList.filter((item) => item != username);
+        chrome.storage.sync.set(
+          { ["blockedAuthors"]: tmpUserList },
+          function () {}
+        );
         this.updateLocalUserList();
-    }
-}
+      });
+    },
+    addUserToChromeStorage() {
+      if (this.inputUsername.trim()) {
+        // checking that the input is not empty
+        chrome.storage.sync.get("blockedAuthors", (data) => {
+          const tmpUserList = data["blockedAuthors"] || [];
+          tmpUserList.push(this.inputUsername);
+          chrome.storage.sync.set(
+            { ["blockedAuthors"]: tmpUserList },
+            function () {}
+          );
+          this.inputUsername = ""; // Clear input
+          this.updateLocalUserList();
+        });
+      }
+    },
+    updateLocalUserList() {
+      chrome.storage.sync.get("blockedAuthors", (data) => {
+        const tmpUserList = data["blockedAuthors"] || [];
+        this.localUserList = tmpUserList;
+      });
+    },
+  },
+  watch: {
+    inputUsername(value) {
+      if (value.length >= 30) {
+        this.inputUsername = this.inputUsername.slice(0, 30); // max length is 30
+      }
+    },
+  },
+  mounted: function () {
+    this.updateLocalUserList();
+  },
+};
 
-Vue.createApp(App).mount("#popup")
+Vue.createApp(App).mount("#popup");
